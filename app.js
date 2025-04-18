e9879bec0efc3e35f0b4cd5d10ac44860c68290c
santhosh <web20design20@gmail.com>
2025-04-18 12:11:11 +0530

    changes in messages

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const name = "Code test";
 
 app.get("/", (req, res) => {
-  res.send("Welcome to code flow");
+  const message = `Welcome to shopyfy ${name}`;
+  res.send(message);
 });
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-module.exports = { app, server };
+module.exports = { app, server, name };
--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,11 @@
 const request = require("supertest");
-const { app, server } = require("../app");
+const { app, server, name } = require("../app");
 
 describe("GET /", () => {
   it("should return 200 status and the correct message", async () => {
     const response = await request(app).get("/");
     expect(response.status).toBe(200);
-    expect(response.text).toBe("Welcome to shopyfy");
+    expect(response.text).toBe(`Welcome to shopyfy ${name}`);
   });
 });
 
